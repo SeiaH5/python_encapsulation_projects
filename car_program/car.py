39c4ee8aa211e0159fbc29f62a08a190957afd9c
unknown <Josiah Calderon>
2026-06-05 17:24:13 +0800

    Added limitation in year model.

--- car_program/car.py
+++ car_program/car.py
@@ -1,6 +1,15 @@
+from datetime import datetime
+
 class Car:
+    
     def __init__(self, year_model, make):
-        self.__year_model = year_model
+        current_year = datetime.now().year
+
+        if 1886 <= year_model <= current_year:
+            self.__year_model = year_model
+        else:
+            raise ValueError("\n\033[31m\nInput a valid year model.\033[0m\n")
+
         self.__make = make
         self.__speed = 0
 
