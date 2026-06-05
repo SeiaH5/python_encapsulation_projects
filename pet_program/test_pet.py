af49048a0f4279cb5a4d99c7ac8b89e991e03066
unknown <Josiah Calderon>
2026-06-05 17:53:27 +0800

    Minor changes: Changed output design.

--- pet_program/test_pet.py
+++ pet_program/test_pet.py
@@ -1,6 +1,5 @@
 from pet import Pet
 
-
 print("\033[32m===== PET'S INFORMATION FORM =====\033[0m")
 
 count_pets = int(input("Enter number of pets\033[2m\033[3m(Example: 2)\033[0m: "))
@@ -10,7 +9,7 @@ pets = []
 for i in range(count_pets):
     my_pet = Pet()
     
-    print(f"\nPet #{i+1} Information")
+    print(f"\n\033[1mPet #{i+1} Information\033[0m")
     pet_name = input("Enter your pet's name\033[2m\033[3m(Example: Jinggoy)\033[0m: ")
     animal_type = input("Enter your pet's animal type\033[2m\033[3m(e.g. Dog, Cat, Bird)\033[0m: ")
     pet_age = int(input("Enter your pet's age in months\033[2m\033[3m(Example: 10)\033[0m: "))
@@ -21,9 +20,9 @@ for i in range(count_pets):
 
     pets.append(my_pet)
 
-print("\n\033[1m===== YOUR PET'S INFORMATION =====\033[0m")
+print("\n"*3, "\033[1m===== YOUR PETS' INFORMATION =====\033[0m")
 for pet in pets:
-    print(f"\033[1m\033[32m{pet.get_name()}\033[0m")
-    print("Type:", pet.get_animal_type())
-    print("Age:", pet.get_age())
-    print("-"*10)
+    print(f"\033[1m\033[34m{pet.get_name()}\033[0m")
+    print("Animal Type:", pet.get_animal_type())
+    print(f"Age: {pet.get_age()} months")
+    print("-"*20)
