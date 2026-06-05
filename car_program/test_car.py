f400f7055f09229cc207e93909eaa2c731d643b8
unknown <Josiah Calderon>
2026-06-05 17:26:01 +0800

    Added delay when accelerating or braking. Improved UI. Organized lines. +minor changes.

--- car_program/test_car.py
+++ car_program/test_car.py
@@ -1,15 +1,29 @@
 from car import Car
+import time 
 
 try:
+    print("=====CAR=====")
+    
     year = int(input("Enter year model: "))
-    make = input("Enter Brand: ")
+    make = input("Enter Make/Brand: ")
 
     car = Car(year, make)
     
-    print("\nStarting")
+    print(f"Current Speed: {car.get_speed()}\n")
 
-    print("Current Speed:", car.get_speed())
+    for i in range(5):
+        car.accelerate()
+        print("\033[2m\033[3mAccelerating\033[0m")
+        print(f"Current Speed: {car.get_speed()}")
+        time.sleep(1)
 
+    print()
 
-except:
-    print("\033[31mError\033[0m")
+    for i in range(5):
+        car.brake()
+        print("\033[2m\033[3mBraking\033[0m")
+        print(f"Current Speed: {car.get_speed()}")
+        time.sleep(1)
+
+except Exception as e:
+    print(f"\n\033[31mError: {e}\033[0m\n")
