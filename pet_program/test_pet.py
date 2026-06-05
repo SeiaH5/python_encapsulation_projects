212db0e29bdda1228c7076129bcd077aab90972f
unknown <Josiah Calderon>
2026-06-05 22:25:01 +0800

    Added exception handling.

--- pet_program/test_pet.py
+++ pet_program/test_pet.py
@@ -1,28 +1,32 @@
 from pet import Pet
 
-print("\033[32m===== PET'S INFORMATION FORM =====\033[0m")
+try:
+    print("\033[32m===== PET'S INFORMATION FORM =====\033[0m")
 
-count_pets = int(input("Enter number of pets\033[2m\033[3m(Example: 2)\033[0m: "))
+    count_pets = int(input("Enter number of pets\033[2m\033[3m(Example: 2)\033[0m: "))
 
-pets = []
+    pets = []
 
-for i in range(count_pets):
-    my_pet = Pet()
-    
-    print(f"\n\033[1mPet #{i+1} Information\033[0m")
-    pet_name = input("Enter your pet's name\033[2m\033[3m(Example: Jinggoy)\033[0m: ")
-    animal_type = input("Enter your pet's animal type\033[2m\033[3m(e.g. Dog, Cat, Bird)\033[0m: ")
-    pet_age = int(input("Enter your pet's age in months\033[2m\033[3m(Example: 10)\033[0m: "))
+    for i in range(count_pets):
+        my_pet = Pet()
+        
+        print(f"\n\033[1mPet #{i+1} Information\033[0m")
+        pet_name = input("Enter your pet's name\033[2m\033[3m(Example: Jinggoy)\033[0m: ")
+        animal_type = input("Enter your pet's animal type\033[2m\033[3m(e.g. Dog, Cat, Bird)\033[0m: ")
+        pet_age = int(input("Enter your pet's age in months\033[2m\033[3m(Example: 10)\033[0m: "))
 
-    my_pet.set_name(pet_name)
-    my_pet.set_animal_type(animal_type)
-    my_pet.set_age(pet_age)
+        my_pet.set_name(pet_name)
+        my_pet.set_animal_type(animal_type)
+        my_pet.set_age(pet_age)
 
-    pets.append(my_pet)
+        pets.append(my_pet)
 
-print("\n"*3, "\033[1m===== YOUR PETS' INFORMATION =====\033[0m")
-for pet in pets:
-    print(f"\033[1m\033[34m{pet.get_name()}\033[0m")
-    print("Animal Type:", pet.get_animal_type())
-    print(f"Age: {pet.get_age()} months")
-    print("-"*20)
+    print("\n"*3, "\033[1m===== YOUR PETS' INFORMATION =====\033[0m")
+    for pet in pets:
+        print(f"\033[1m\033[34m{pet.get_name()}\033[0m")
+        print("Animal Type:", pet.get_animal_type())
+        print(f"Age: {pet.get_age()} months")
+        print("-"*20)
+
+except Exception as e:
+    print(f"\n\033[31mError: {e}\033[0m\n")
