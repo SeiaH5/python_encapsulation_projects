3cad1e0d0c5d0c2258316e56d2b764dceed46767
unknown <Josiah Calderon>
2026-06-05 16:44:25 +0800

    Major Changes: Consistent program. No inputs but direct to the instruction. More clear and much easier to read.

---
+++ fan_program/test_fan.py
@@ -0,0 +1,19 @@
+from fan import Fan
+
+fan1 = Fan()
+fan1.set_speed(Fan.FAST)
+fan1.set_radius(10)
+fan1.set_color("Yellow")
+fan1.set_on(True)
+
+fan2 = Fan()
+fan2.set_speed(Fan.MEDIUM)
+fan2.set_radius(10)
+fan2.set_color("Yellow")
+fan2.set_on(True)
+
+print("\033[32m===== FIRST FAN =====\033[0m")
+fan1.display_specs()
+
+print("\n\033[33m===== SECOND FAN =====\033[0m")
+fan2.display_specs()
